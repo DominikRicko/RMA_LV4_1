2dd7c6f44a075f76588ad1f7a5dcd5cbef9f88ca
Dominik Ričko <dominik.ricko@gmail.com>
2021-04-30 18:29:54 +0200

    added application context

---
+++ app/src/main/java/hr/dominikricko/rma_lv3_1/context/ApplicationContext.kt
@@ -0,0 +1,19 @@
+package hr.dominikricko.rma_lv3_1.context
+
+import android.app.Application
+import android.content.Context
+
+class ApplicationContext : Application() {
+
+    companion object {
+        lateinit var instance: ApplicationContext
+
+        val context: Context
+            get() = instance.applicationContext
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
